4be907b7322a47936eea174a29c89a02b80c98cc
netotirj <netotirj@gmail.com>
2026-05-12 16:49:01 -0500

    Allow microphone access for WhatsApp WebRTC

--- app/Http/Response.php
+++ app/Http/Response.php
@@ -58,7 +58,7 @@ class Response
             'X-Frame-Options' => 'SAMEORIGIN',
             'X-Content-Type-Options' => 'nosniff',
             'Referrer-Policy' => 'strict-origin-when-cross-origin',
-            'Permissions-Policy' => 'camera=(), microphone=(), geolocation=()',
+            'Permissions-Policy' => 'camera=(self), microphone=(self), geolocation=()',
         ];
 
         $isHttps = (

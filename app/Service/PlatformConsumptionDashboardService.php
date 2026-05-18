b48ae6a9a0062bfcfb583d2c78fb1d7b4c0191ed
netotirj <netotirj@gmail.com>
2026-05-18 10:38:21 -0500

    Drop callback received_at from global costs

--- app/Service/PlatformConsumptionDashboardService.php
+++ app/Service/PlatformConsumptionDashboardService.php
@@ -1784,7 +1784,7 @@ class PlatformConsumptionDashboardService
         $prefix = $alias !== '' ? $alias . '.' : '';
         $parts = [];
 
-        foreach (['received_at', 'webhook_created', 'created_at'] as $column) {
+        foreach (['webhook_created', 'created_at'] as $column) {
             if (self::columnExists('callback', $column)) {
                 $parts[] = $prefix . $column;
             }
